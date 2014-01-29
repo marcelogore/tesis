66252dab3154da8fb627ebb8f253b67b78d6ad71
Marcelo Gore <marcelogore@gmail.com>
2014-01-29 00:29:28 -0300

    Más escenarios

--- boids/src/main/java/ar/com/marcelogore/tesis/boids/BoidsApplication.java
+++ boids/src/main/java/ar/com/marcelogore/tesis/boids/BoidsApplication.java
@@ -8,8 +8,7 @@ import javafx.stage.Stage;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import ar.com.marcelogore.tesis.boids.scenes.IntersectionScenario;
-import ar.com.marcelogore.tesis.boids.scenes.Scenario;
+import ar.com.marcelogore.tesis.boids.scenes.*;
 
 public class BoidsApplication extends Application {
 
@@ -22,9 +21,15 @@ public class BoidsApplication extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 
+//		final Scenario scenario =  new PlainScenario();
 //		final Scenario scenario =  new SimpleObstacleScenario();
 //		final Scenario scenario =  new FunnelScenario();
-		final Scenario scenario =  new IntersectionScenario();
+//		final Scenario scenario =  new IntersectionScenario();
+//		final Scenario scenario =  new MultipleIntersectionScenario();
+//		final Scenario scenario =  new LaneContractionScenario();
+//		final Scenario scenario =  new IntersectionCollisionScenario();
+		final Scenario scenario =  new FrontCollisionScenario();
+
 		final Scene scene = scenario.createScene();
 
 		AnimationTimer timer = new AnimationTimer() {
