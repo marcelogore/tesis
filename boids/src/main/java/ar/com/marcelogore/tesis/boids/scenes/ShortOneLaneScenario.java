edd079ac4a0cce0b22120f0896425a5d72d0b870
Marcelo Gore <marcelogore@gmail.com>
2014-06-10 13:45:58 -0300

    Tiny example

---
+++ boids/src/main/java/ar/com/marcelogore/tesis/boids/scenes/ShortOneLaneScenario.java
@@ -0,0 +1,67 @@
+package ar.com.marcelogore.tesis.boids.scenes;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import ar.com.marcelogore.tesis.boids.Boid;
+import ar.com.marcelogore.tesis.boids.CircularBoid;
+import ar.com.marcelogore.tesis.boids.util.ScenaryCreator;
+import ar.com.marcelogore.tesis.boids.util.Vector;
+
+public class ShortOneLaneScenario extends Scenario {
+
+	public ShortOneLaneScenario(Integer numberOfBoids) {
+		this.setNumberOfBoids(numberOfBoids);
+	}
+	
+	@Override
+	public Scene createScene() {
+
+		List<Boid> boids = new ArrayList<Boid>();
+		
+		// Vuelen a la banda contraria
+		final Vector goalA = new Vector(this.getSceneSize().x, 10);
+		final Vector goalB = new Vector(this.getSceneSize().x, 50);
+
+		for (int j = 0; j < this.getNumberOfBoids(); j++) {
+			
+			Boid boid = Boid.createRandomBoid(0, 10, 100, 40);
+			boid.setName("Boid" + j);
+			boid.setGoal(goalA, goalB);
+			
+			boids.add(boid);
+			boid.setOtherBoids(boids);
+			
+			boid.setMaxX((int) getSceneSize().x);
+			boid.setMaxY((int) getSceneSize().y);
+
+			representedBoids.add(new CircularBoid(boid));
+		}
+
+		List<CircularBoid> obstacles = new LinkedList<CircularBoid>();
+		
+		// Ancho inicial
+		ScenaryCreator.drawLine(obstacles, new Vector(0,5), new Vector(200,5));
+		ScenaryCreator.drawLine(obstacles, new Vector(0,55), new Vector(200,55));
+		
+		representedBoids.addAll(obstacles);
+		for (CircularBoid cBoid : obstacles) {
+			boids.add(cBoid.getBoid());
+		}
+		
+		final Group group = new Group(representedBoids.toArray(new Circle[0]));
+
+		return new Scene(group, getSceneSize().x, getSceneSize().y, Color.WHITE);
+	}
+
+	@Override
+	public Vector getSceneSize() {
+		return new Vector(200,60);
+	}
+
+}
